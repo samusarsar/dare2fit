ed46ac04a1c04b90dc8830392317d8cc60ac0d04
Simona Ilcheva <sdmileva@gmail.com>
2023-05-20 17:38:37 +0300

    Style(#3): style RootLayout as grid

--- dare2fit/src/views/RootLayout/RootLayout.tsx
+++ dare2fit/src/views/RootLayout/RootLayout.tsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import { Outlet } from 'react-router';
 
-import NavBar from '../../components/Base/NavBar.jsx';
+import NavBar from '../../components/Navigation/NavBar.js';
 import Footer from '../../components/Base/Footer.jsx';
+import { Grid, GridItem } from '@chakra-ui/react';
 
 
 const RootLayout: React.FC = () => {
     return (
         <main>
-            <NavBar />
-            <Outlet />
-            <Footer />
+            <Grid height={'100vh'}
+                templateAreas={`"nav main"
+                                "nav footer"`}
+                gridTemplateRows={'1fr 5%'}
+                gridTemplateColumns='min-content 1fr'>
+                <GridItem area={'nav'}>
+                    <NavBar />
+                </GridItem>
+
+                <GridItem area={'main'}>
+                    <Outlet />
+                </GridItem>
+
+                <GridItem area={'footer'}>
+                    <Footer />
+                </GridItem>
+            </Grid>
         </main>
     );
 };
