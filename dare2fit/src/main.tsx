282601263565b4c0f4ed564483a85892da5d3546
samu.sar <samuil.sarandev@gmail.com>
2023-05-20 10:14:34 +0300

    Chore(#1): extend chakra-ui theme with brand colors

--- dare2fit/src/main.tsx
+++ dare2fit/src/main.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { extendTheme, ChakraProvider } from '@chakra-ui/react';
+
+const theme = extendTheme({
+    colors: {
+        brand: {
+            light: '#FFFFFF',
+            dark: '#1A202C',
+            red: '#F24968',
+            blue: '#6929F2',
+            purple: '#9B72F2',
+            green: '#14D990',
+            yellow: '#F2B807',
+        },
+    },
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+    <ChakraProvider theme={theme} >
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </ChakraProvider>,
 );
