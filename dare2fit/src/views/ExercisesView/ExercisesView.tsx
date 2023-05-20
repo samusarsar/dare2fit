8ad65850040a64d89d3c93370ee7f91ed01d628f
samu.sar <samuil.sarandev@gmail.com>
2023-05-20 21:15:05 +0300

    Add(#11): add initial base ExercisesView

--- dare2fit/src/views/ExercisesView/ExercisesView.tsx
+++ dare2fit/src/views/ExercisesView/ExercisesView.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import { FC, ReactElement } from 'react';
+import { VStack, useColorModeValue } from '@chakra-ui/react';
+import CreateExercise from '../../components/Exercises/CreateExercise/CreateExercise';
 
-const ExercisesView: React.FC = () => {
+const ExercisesView: FC = (): ReactElement => {
     return (
-        <p>Exercises View</p>
+        <VStack p={5} bg={useColorModeValue('brand.light', 'brand.dark')}>
+            <CreateExercise />
+        </VStack>
     );
 };
 
