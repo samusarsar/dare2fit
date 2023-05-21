3da947eb266dae795974364b693495d9e6c0032b
samu.sar <samuil.sarandev@gmail.com>
2023-05-21 17:48:57 +0300

    Add(#14): add CreateExercise component

--- dare2fit/src/components/Exercises/CreateExercise/CreateExercise.tsx
+++ dare2fit/src/components/Exercises/CreateExercise/CreateExercise.tsx
@@ -17,9 +17,15 @@ import {
     VStack,
     Grid,
     FormErrorMessage,
+    HStack,
+    Icon,
+    Image,
+    Spacer,
+    useToast,
 } from '@chakra-ui/react';
 import { Formik, Field } from 'formik';
 import { MdArrowDropDown } from 'react-icons/md';
+import { IoMdAdd } from 'react-icons/io';
 import { ACCEPTED_IMAGE_TYPES, EXERCISE_NAME_MAX_LENGTH } from '../../../common/constants';
 import { EXERCISE_NAME_MIN_LENGTH } from '../../../common/constants';
 import { addExercise } from '../../../services/exercise.services';
@@ -29,27 +35,34 @@ const CreateExercise: FC = (): ReactElement => {
     const { userData } = useContext(AppContext);
 
     const [image, setImage] = useState<File | null>(null);
-    const [imageError, setImageError] = useState<boolean>(false);
+    const [imageError, setImageError] = useState(false);
+    const [creating, setCreating] = useState(false);
 
     const { isOpen, onOpen, onClose } = useDisclosure();
+    const toast = useToast();
 
     const handleImageSelect = (e: ChangeEvent<HTMLInputElement>): void => {
-        setImageError(!ACCEPTED_IMAGE_TYPES.includes(e.target.files[0].type));
+        if (e.target.files) {
+            const file: File = e.target.files[0];
+            setImageError(!ACCEPTED_IMAGE_TYPES.includes(file.type));
 
-        if (ACCEPTED_IMAGE_TYPES.includes(e.target.files[0].type)) {
-            setImage(e.target.files[0]);
+            if (ACCEPTED_IMAGE_TYPES.includes(file.type)) {
+                setImage(file);
+            }
         }
     };
 
     return (
-        <>
-            <Button variant='outline' border='4px solid' borderColor='brand.blue' w='80%' h='100px'
-                onClick={onOpen}>Open Modal</Button>
-
+        <HStack justify='center' w='100%' p={4}>
+            <Button variant='ghost' w='80%' h='100px' colorScheme='purple'
+                onClick={onOpen}><Icon as={IoMdAdd} boxSize={6} mr={2}/>Create Exercise</Button>
             <Modal
                 isOpen={isOpen}
-                onClose={onClose}
-            >
+                onClose={() => {
+                    onClose();
+                    setImage(null);
+                    setImageError(false);
+                }}>
                 <ModalOverlay />
                 <Formik
                     initialValues={{
@@ -62,20 +75,48 @@ const CreateExercise: FC = (): ReactElement => {
                     validateOnChange={false}
                     validateOnBlur={false}
                     onSubmit={(values) => {
-                        addExercise(values.exerciseName, userData.handle, values.type, values.difficulty, values.instructions, image);
-                        setImage(null);
+                        setCreating(true);
+                        addExercise(values.exerciseName, userData.handle, values.type, values.difficulty, values.instructions, image)
+                            .then(() => {
+                                setImage(null);
+                                onClose();
+                                toast({
+                                    title: 'Exercise created successfully!',
+                                    description: 'You have added a new exercise.',
+                                    status: 'success',
+                                    duration: 3000,
+                                    isClosable: true,
+                                    position: 'top',
+                                    variant: 'subtle',
+                                });
+                            })
+                            .catch(() => {
+                                toast({
+                                    title: 'Error creating exercise!',
+                                    description: 'Please try again later.',
+                                    status: 'error',
+                                    duration: 3000,
+                                    isClosable: true,
+                                    position: 'top',
+                                    variant: 'subtle',
+                                });
+                            })
+                            .finally(() => {
+                                setCreating(false);
+                            });
                     }}
                 >
                     {({ handleSubmit, errors, touched }) => (
                         <form onSubmit={handleSubmit}>
                             <ModalContent
-                                bg='brand.blue'
+                                px={6}
+                                py={4}
+                                bg='brand.purple'
                                 color='brand.light'>
                                 <ModalHeader>Create a new exercise</ModalHeader>
                                 <ModalCloseButton />
                                 <ModalBody pb={6}>
                                     <VStack>
-
                                         <FormControl isInvalid={!!errors.exerciseName && touched.exerciseName} isRequired={true}>
                                             <FormLabel>Exercise Name</FormLabel>
                                             <Field as={Input} id='exerciseName' name='exerciseName' type='text' placeholder='Jogging' bg='brand.light' color='brand.dark'
@@ -84,12 +125,12 @@ const CreateExercise: FC = (): ReactElement => {
                                                         'Exercise name must be between 4 and 30 characters.' :
                                                         null;
                                                 }}/>
-                                            <FormErrorMessage>{errors.exerciseName}</FormErrorMessage>
+                                            <FormErrorMessage bg='whiteAlpha.700' p={1} rounded='md'>{errors.exerciseName}</FormErrorMessage>
                                         </FormControl>
                                         <Grid templateColumns='1fr 1fr' gap={2}>
                                             <FormControl isRequired={true}>
                                                 <FormLabel htmlFor='type'>Exercise Type</FormLabel>
-                                                <Field as={Select} icon={<MdArrowDropDown />} placeholder='Select exercise type' bg='brand.light' color='brand.dark'
+                                                <Field as={Select} icon={<MdArrowDropDown />} placeholder='Select type' bg='brand.light' color='brand.dark'
                                                     id='type' name='type'>
                                                     <optgroup label='Lifestyle Sports'>
                                                         <option value='walking'>Walking</option>
@@ -123,25 +164,34 @@ const CreateExercise: FC = (): ReactElement => {
                                         </Grid>
 
                                         <FormControl>
-                                            <FormLabel htmlFor='instructions'>Instructions (optional)</FormLabel>
+                                            <FormLabel htmlFor='instructions'>Instructions <i>(optional)</i></FormLabel>
                                             <Field as={Textarea} placeholder='Provide details on how to do this exercise.' bg='brand.light' color='brand.dark'
                                                 id='instructions' name='instructions' />
                                         </FormControl>
-                                        <FormControl isInvalid={imageError}>
-                                            <FormLabel htmlFor='upload'>Image (optional)</FormLabel>
-                                            <Input id='upload' name='upload' type='file'
-                                                onChange={handleImageSelect} />
-                                            <FormErrorMessage>Images should be in still image format.</FormErrorMessage>
-                                        </FormControl>
+                                        <HStack w='100%' justify='start' align='start'>
+                                            <FormControl isInvalid={imageError}>
+                                                <FormLabel>Image <i>(optional)</i></FormLabel>
+                                                <Button as={FormLabel} htmlFor='upload' colorScheme='whiteAlpha'>Upload</Button>
+                                                <Input id='upload' name='upload' type='file' display='none'
+                                                    onChange={handleImageSelect} />
+                                                <FormErrorMessage bg='whiteAlpha.700' p={1} rounded='md'>Images should be in still image format.</FormErrorMessage>
+                                            </FormControl>
+                                            <Spacer />
+                                            {image && <Image src={URL.createObjectURL(image)} boxSize='180px' objectFit='cover'/>}
+                                        </HStack>
                                     </VStack>
                                 </ModalBody>
 
                                 <ModalFooter>
                                     <VStack w='100%' align='center'>
-                                        <Button w='40%' colorScheme='yellow' type='submit'>
+                                        <Button w='40%' colorScheme='yellow' type='submit' isLoading={creating} loadingText='Creating'>
                                             Create Exercise
                                         </Button>
-                                        <Button w='40%' colorScheme='blackAlpha' onClick={onClose}>Cancel</Button>
+                                        <Button w='40%' colorScheme='blackAlpha' onClick={() => {
+                                            onClose();
+                                            setImage(null);
+                                            setImageError(false);
+                                        }}>Cancel</Button>
                                     </VStack>
                                 </ModalFooter>
                             </ModalContent>
@@ -149,7 +199,7 @@ const CreateExercise: FC = (): ReactElement => {
                     )}
                 </Formik>
             </Modal>
-        </>
+        </HStack>
     );
 };
 
