5edfed1e6cb8316fe2867668874326b1d3964c7e
samu.sar <samuil.sarandev@gmail.com>
2023-05-23 13:33:42 +0300

    Add(#7): add initial GoalsView

--- dare2fit/src/views/GoalsView/GoalsView.tsx
+++ dare2fit/src/views/GoalsView/GoalsView.tsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { AppContext } from '../../context/AppContext/AppContext';
+import { getGoalsByHandle } from '../../services/goal.services';
+import { HStack } from '@chakra-ui/layout';
+import SingleHabit from '../../components/Goals/SingleHabit/SingleHabit';
+import GoalList from '../../components/Goals/GoalList/GoalList';
 
 const GoalsView: React.FC = () => {
+    const { userData } = useContext(AppContext);
+
+    const [habits, setHabits] = useState<[] | null>(null);
+    const [challenges, setChallenges] = useState<[] | null>(null);
+
+    useEffect(() => {
+        if (userData) {
+            getGoalsByHandle(userData.handle)
+                .then(data => {
+                    setHabits(Object.values(data).filter(goal => goal.repeat));
+                    setChallenges(Object.values(data).filter(goal => goal.duration));
+                })
+                .catch(() => {
+                    setHabits([]);
+                    setChallenges([]);
+                });
+        }
+    }, [userData]);
+
     return (
-        <p>Goals View</p>
+        <HStack gap={2} flexWrap='wrap'>
+            <GoalList goals={habits} heading='My Habits:' SingleGoal={SingleHabit} />
+        </HStack>
     );
 };
 
