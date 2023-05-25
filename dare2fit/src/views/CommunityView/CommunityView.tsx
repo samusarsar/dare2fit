36600a608913525bacb8d1df7cdc7a950e0e3497
samu.sar <samuil.sarandev@gmail.com>
2023-05-25 15:07:38 +0300

    Style(#22): style community tab section headers

--- dare2fit/src/views/CommunityView/CommunityView.tsx
+++ dare2fit/src/views/CommunityView/CommunityView.tsx
@@ -1,6 +1,6 @@
 import { FC, ReactElement } from 'react';
 import SearchUsers from '../../components/Users/SearchUsers/SearchUsers';
-import { Accordion, AccordionItem, AccordionButton, Box, AccordionIcon, AccordionPanel, VStack } from '@chakra-ui/react';
+import { Accordion, AccordionItem, AccordionButton, Box, AccordionPanel, VStack, Heading } from '@chakra-ui/react';
 import FriendsList from '../../components/Users/FriendsList/FriendsList';
 import FriendRequestsList from '../../components/Users/FriendRequests/FriendRequests';
 import { FriendRequestType } from '../../common/enums';
@@ -13,10 +13,7 @@ const CommunityView: FC = (): ReactElement => {
                 <h2>
                     <AccordionItem>
                         <AccordionButton h='80px' _expanded={{ color: 'brand.yellow' }}>
-                            <Box as="span" flex='1' textAlign='left' fontWeight='bold' fontSize='1.2em'>
-                                    My Community:
-                            </Box>
-                            <AccordionIcon />
+                            <Heading as='h2' size='lg'>My Community:</Heading>
                         </AccordionButton>
                         <AccordionPanel pb={4}>
                             <FriendsList />
@@ -27,10 +24,7 @@ const CommunityView: FC = (): ReactElement => {
                 <h2>
                     <AccordionItem>
                         <AccordionButton h='80px' _expanded={{ color: 'brand.yellow' }}>
-                            <Box as="span" flex='1' textAlign='left' fontWeight='bold' fontSize='1.2em'>
-                                    Friend Requests:
-                            </Box>
-                            <AccordionIcon />
+                            <Heading as='h2' size='lg'>Friend Requests:</Heading>
                         </AccordionButton>
                         <AccordionPanel pb={4}>
                             <VStack w='100%' gap={3}>
