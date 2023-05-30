118670278771c1f0ddd0a6ccfa7720dabea9cba9
Simona Ilcheva <sdmileva@gmail.com>
2023-05-30 18:32:26 +0300

    Add(#37): add activity tab carousel

--- dare2fit/src/components/Activity/ActivityCarousel/WorkoutsCarousel.tsx
+++ dare2fit/src/components/Activity/ActivityCarousel/WorkoutsCarousel.tsx
@@ -1,10 +1,10 @@
 import { FC, useContext, useEffect, useState } from 'react';
 
-import { HStack, Heading, Select, Text } from '@chakra-ui/react';
+import { Box, HStack, Heading, Select, Text, VStack } from '@chakra-ui/react';
 
 import { AppContext } from '../../../context/AppContext/AppContext';
 import { IWorkout } from '../../../common/types';
-import { getWorkoutsByHandle } from '../../../services/workout.services';
+import { getWorkoutsByHandle, sortWorkoutsByDate } from '../../../services/workout.services';
 import ActivityCarousel from './ActivityCarousel';
 import WorkoutDetails from '../../Workouts/WorkoutDetails';
 
@@ -12,36 +12,56 @@ const WorkoutsCarousel: FC = () => {
     const { userData } = useContext(AppContext);
 
     const [myWorkouts, setMyWorkouts] = useState<IWorkout[] | []>([]);
+    const [savedWorkouts, setSavedWorkouts] = useState<IWorkout[] | []>([]);
+    const [workoutsView, setWorkoutsView] = useState('my');
     const [index, setIndex] = useState(0);
 
     useEffect(() => {
         getWorkoutsByHandle(userData!.handle)
-            .then(setMyWorkouts)
+            .then(workouts => {
+                const myWo = sortWorkoutsByDate(workouts.filter(w => w.author === userData!.handle));
+                setMyWorkouts(myWo);
+                const savedWo = sortWorkoutsByDate(workouts.filter(w => w.author !== userData!.handle));
+                setSavedWorkouts(savedWo);
+            })
             .catch(() => setMyWorkouts([]));
     }, [userData]);
 
-    if (!myWorkouts.length) {
-        return (
-            <Text>You don&apos;t have workouts, yet...</Text>
-        );
-    }
-
     return (
-        <ActivityCarousel setIndex={setIndex} index={index} length={myWorkouts.length}>
-            <>
-                <HStack align='space-between' gap={2} mb={2}>
-                    <Heading as='h2' size='lg'>Workouts</Heading>
-                    <Select
-                    // onChange={(e) => setFilter(e.target.value)}
-                    >
-                        <option value='myWorkouts'>My Workouts</option>
-                        <option value='savedWorkouts'>Saved Workouts</option>
-                    </Select>
-                </HStack>
-                <WorkoutDetails key={myWorkouts[index].workoutId} workout={myWorkouts[index]} />
-            </>
-        </ActivityCarousel>
+        <VStack px={1} align='start' rounded='lg'>
+            <HStack align='space-between' gap={2} mb={2}>
+                <Heading as='h2' size='lg'>Workouts</Heading>
+                <Select
+                    onChange={(e) => setWorkoutsView(e.target.value)}>
+                    <option value='my'>My Workouts</option>
+                    <option value='saved'>Saved Workouts</option>
+                </Select>
+            </HStack>
 
+            <VStack align='start' rounded='lg'>
+                <Box height='300px' overflow='auto'>
+                    {workoutsView === 'my' && !myWorkouts.length ? (
+                        <Text>You don&apos;t have workouts, yet...</Text>
+                    ) : workoutsView === 'my' ? (
+                        <>
+                            <ActivityCarousel setIndex={setIndex} index={index} length={myWorkouts.length}></ActivityCarousel>
+                            <WorkoutDetails workout={myWorkouts[index]} />
+
+                        </>
+
+                    ) : workoutsView === 'saved' && !savedWorkouts.length ? (
+                        <Text>You don&apos;t have saved workouts, yet...</Text>
+                    ) : (
+                        <>
+                            <ActivityCarousel setIndex={setIndex} index={index} length={savedWorkouts.length}></ActivityCarousel>
+                            <WorkoutDetails workout={savedWorkouts[index]} />
+
+                        </>
+
+                    )}
+                </Box>
+            </VStack>
+        </VStack >
     );
 };
 
--- dare2fit/src/views/ActivityView/ActivityView.tsx
+++ dare2fit/src/views/ActivityView/ActivityView.tsx
@@ -4,13 +4,14 @@ import { Grid, VStack } from '@chakra-ui/react';
 
 import ActivityLogger from '../../components/Activity/ActivityLogger/ActivityLogger';
 import WorkoutsCarousel from '../../components/Activity/ActivityCarousel/WorkoutsCarousel';
+import GoalsCarousel from '../../components/Activity/ActivityCarousel/GoalsCarousel';
 
 const ActivityView: FC = (): ReactElement => {
     return (
         <VStack>
-            <Grid templateColumns='1fr 1fr 1fr'>
+            <Grid templateColumns='1fr 1fr 1fr' >
                 <WorkoutsCarousel />
-
+                <GoalsCarousel />
             </Grid>
 
             <ActivityLogger />
