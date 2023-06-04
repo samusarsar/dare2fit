ed882da982af0c723a0b9edc478b22cc87b4f80b
samu.sar <samuil.sarandev@gmail.com>
2023-06-04 16:46:57 +0300

    Fix(#47): navigate upon notification removal bug

--- src/components/Navigation/Notifications/SingleNotification/SingleNotification.tsx
+++ src/components/Navigation/Notifications/SingleNotification/SingleNotification.tsx
@@ -1,7 +1,7 @@
 import { Icon } from '@chakra-ui/icon';
 import { HStack, Spacer, Text } from '@chakra-ui/layout';
 import { As } from '@chakra-ui/system';
-import { FC, ReactElement, useContext } from 'react';
+import { FC, MouseEvent, ReactElement, useContext } from 'react';
 import { MdRemoveCircle } from 'react-icons/md';
 import { removeNotification } from '../../../../services/notification.services';
 import { AppContext } from '../../../../context/AppContext/AppContext';
@@ -9,7 +9,8 @@ import { AppContext } from '../../../../context/AppContext/AppContext';
 const SingleNotification: FC<{ notification: string, timestamp?: string, icon: As}> = ({ notification, timestamp, icon }): ReactElement => {
     const { userData } = useContext(AppContext);
 
-    const handleRemove = () => {
+    const handleRemove = (e: MouseEvent) => {
+        e.stopPropagation();
         removeNotification(userData!.handle, timestamp!);
     };
 
@@ -19,7 +20,7 @@ const SingleNotification: FC<{ notification: string, timestamp?: string, icon: A
             <Text>{notification}</Text>
             <Spacer />
             {timestamp &&
-            <HStack p={2} transition='0.2s ease-in' _hover={{ bg: 'brand.red' }} rounded='lg'>
+            <HStack p={2} transition='0.2s ease-in' _hover={{ bg: 'brand.red' }} rounded='lg' zIndex={2}>
                 <Icon as={MdRemoveCircle} onClick={handleRemove} />
             </HStack>}
         </HStack>
