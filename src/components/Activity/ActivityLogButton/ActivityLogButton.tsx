bdf94032c90f7030b220cff6866a220f2db696b5
samu.sar <samuil.sarandev@gmail.com>
2023-06-05 10:42:33 +0300

    Add(#38): add joint-training logging functionality

--- src/components/Activity/ActivityLogButton/ActivityLogButton.tsx
+++ src/components/Activity/ActivityLogButton/ActivityLogButton.tsx
@@ -1,5 +1,5 @@
 // eslint-disable-next-line max-len
-import { Box, Button, ButtonGroup, Collapse, HStack, IconButton, InputGroup, InputRightAddon, NumberDecrementStepper, NumberIncrementStepper, NumberInput, NumberInputField, NumberInputStepper, Select, useDisclosure } from '@chakra-ui/react';
+import { Box, Button, ButtonGroup, Collapse, HStack, IconButton, InputGroup, InputRightAddon, NumberDecrementStepper, NumberIncrementStepper, NumberInput, NumberInputField, NumberInputStepper, Select, VStack, useDisclosure } from '@chakra-ui/react';
 import { FC, ReactElement, useContext, useEffect, useState } from 'react';
 import { IoIosArrowUp } from 'react-icons/io';
 import { MdArrowDropDown } from 'react-icons/md';
@@ -17,6 +17,7 @@ const ActivityLogButton: FC<{ todayLog: ITodayLog | null }> = ({ todayLog }): Re
     const [loggedValue, setLoggedValue] = useState<string | number | null>(null);
     const [userWorkouts, setUserWorkouts] = useState<IWorkout[] | []>([]);
     const [savedWorkouts, setSavedWorkouts] = useState<IWorkout[] | []>([]);
+    const [trainingWith, setTrainingWith] = useState<string>('');
 
     const { isOpen, onToggle } = useDisclosure();
 
@@ -30,6 +31,8 @@ const ActivityLogButton: FC<{ todayLog: ITodayLog | null }> = ({ todayLog }): Re
         savedWorkouts :
         savedWorkouts.filter(workout => workout.category === activityType);
 
+    const userFriends = userData!.friends ? Object.keys(userData!.friends) : null;
+
     const workoutIsLogged = todayLog ?
         Object.keys(todayLog).includes('workout') :
         false;
@@ -37,6 +40,11 @@ const ActivityLogButton: FC<{ todayLog: ITodayLog | null }> = ({ todayLog }): Re
     const handleLog = () => {
         if (!!activityType && !!loggedValue) {
             logActivity({ handle: userData!.handle, activityType, loggedValue });
+
+            if (trainingWith.length > 0) {
+                logActivity({ handle: trainingWith, activityType, loggedValue });
+            }
+
             onToggle();
             setActivityType('');
             setLoggedValue(null);
@@ -47,6 +55,7 @@ const ActivityLogButton: FC<{ todayLog: ITodayLog | null }> = ({ todayLog }): Re
         onToggle();
         setActivityType('');
         setLoggedValue(null);
+        setTrainingWith('');
     };
 
     useEffect(() => {
@@ -64,58 +73,70 @@ const ActivityLogButton: FC<{ todayLog: ITodayLog | null }> = ({ todayLog }): Re
     return (
         <Box>
             <Collapse in={isOpen} animateOpacity>
-                <HStack rounded='md' px={1} py={3}>
-                    <Select
-                        icon={<MdArrowDropDown />}
-                        value={activityType}
-                        placeholder="Select activity"
-                        onChange={(e) => setActivityType(e.target.value)}>
-                        <optgroup label="Lifestyle Sports">
-                            <option value="walking">Walking</option>
-                            <option value="running">Running</option>
-                            <option value="cycling">Cycling</option>
-                            <option value="swimming">Swimming</option>
-                        </optgroup>
-                        {!workoutIsLogged &&
-                            <optgroup label="Workouts">
-                                <option value="workout">Any Workout</option>
-                                <option value="strength">Strength</option>
-                                <option value="stamina">Stamina</option>
-                                <option value="stretching">Stretching</option>
-                            </optgroup>}
-                    </Select>
-                    {(activityType && units === 'workouts') ?
-                        (<Select
+                <VStack py={3} w='100%'>
+                    <HStack rounded='md' w='100%'>
+                        <Select
                             icon={<MdArrowDropDown />}
-                            placeholder='Select workout'
-                            onChange={(e) => setLoggedValue(e.target.value)}>
-                            <optgroup label="My Workouts">
-                                {userWorkoutOptions.map(workout =>
-                                    <option key={workout.workoutId} value={`${workout.workoutName}_${workout.category}`}>{workout.workoutName}</option>,
-                                )}
+                            value={activityType}
+                            placeholder="Select activity"
+                            onChange={(e) => setActivityType(e.target.value)}>
+                            <optgroup label="Lifestyle Sports">
+                                <option value="walking">Walking</option>
+                                <option value="running">Running</option>
+                                <option value="cycling">Cycling</option>
+                                <option value="swimming">Swimming</option>
                             </optgroup>
-                            <optgroup label="Saved Workouts">
-                                {savedWorkoutOptions.map(workout =>
-                                    <option key={workout.workoutId} value={`${workout.workoutName}_${workout.category}`}>{workout.workoutName}</option>,
-                                )}
-                            </optgroup>
-                        </Select>) :
-                        (<>
-                            {units &&
-                                (<InputGroup>
-                                    <NumberInput min={1} rounded='lg' onChange={(e) => setLoggedValue(+e)}>
-                                        <NumberInputField />
-                                        <NumberInputStepper>
-                                            <NumberIncrementStepper />
-                                            <NumberDecrementStepper />
-                                        </NumberInputStepper>
-                                    </NumberInput>
-                                    <InputRightAddon>
-                                        {units}
-                                    </InputRightAddon>
-                                </InputGroup>)}
-                        </>)}
-                </HStack>
+                            {!workoutIsLogged &&
+                                <optgroup label="Workouts">
+                                    <option value="workout">Any Workout</option>
+                                    <option value="strength">Strength</option>
+                                    <option value="stamina">Stamina</option>
+                                    <option value="stretching">Stretching</option>
+                                </optgroup>}
+                        </Select>
+                        {(activityType && units === 'workouts') ?
+                            (<Select
+                                icon={<MdArrowDropDown />}
+                                placeholder='Select workout'
+                                onChange={(e) => setLoggedValue(e.target.value)}>
+                                <optgroup label="My Workouts">
+                                    {userWorkoutOptions.map(workout =>
+                                        <option key={workout.workoutId} value={`${workout.workoutName}_${workout.category}`}>{workout.workoutName}</option>,
+                                    )}
+                                </optgroup>
+                                <optgroup label="Saved Workouts">
+                                    {savedWorkoutOptions.map(workout =>
+                                        <option key={workout.workoutId} value={`${workout.workoutName}_${workout.category}`}>{workout.workoutName}</option>,
+                                    )}
+                                </optgroup>
+                            </Select>) :
+                            (<>
+                                {units &&
+                                    (<InputGroup>
+                                        <NumberInput min={1} rounded='lg' onChange={(e) => setLoggedValue(+e)}>
+                                            <NumberInputField />
+                                            <NumberInputStepper>
+                                                <NumberIncrementStepper />
+                                                <NumberDecrementStepper />
+                                            </NumberInputStepper>
+                                        </NumberInput>
+                                        <InputRightAddon>
+                                            {units}
+                                        </InputRightAddon>
+                                    </InputGroup>)}
+                            </>)}
+                    </HStack>
+                    {(userFriends && activityType) &&
+                    <Select
+                        placeholder='Add friend to activity'
+                        icon={<MdArrowDropDown />}
+                        value={trainingWith}
+                        onChange={(e) => setTrainingWith(e.target.value)}>
+                        <optgroup label='Friends'>
+                            {userFriends.map(friend => <option key={friend} value={friend}>{friend}</option>)}
+                        </optgroup>
+                    </Select>}
+                </VStack>
             </Collapse>
             {!isOpen ?
                 <Button w='100%' colorScheme='yellow' onClick={onToggle}>Log Activity</Button> :
